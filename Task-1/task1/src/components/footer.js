a2969ed00ce0a8910fe93739cd59ae19f3a95de5
rose <57836306+roshan1727@users.noreply.github.com>
2021-11-29 23:28:33 +0530

    Task-1 Update

---
+++ Task-1/task1/src/components/footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+//createing footer functional
+const footer =()=>{
+    return(
+        
+        <footer>
+        <div className="center">
+            copyright &copy; classic Designs All rights reserved!
+        </div>
+    </footer>
+        
+    )
+};
+
+export default footer;
---
+++ Task-1/task1/src/components/maincontent.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+const maincontent=()=>{
+    return(
+        <div>
+        <h1 style="text-align: center;">Cloud Migration to Azure</h1>
+        <img src="Task-1\task1\src\images\Picture1.png" alt="picture1" />
+        <p style="padding:27px">
+            <strong>“Migrate to Azure Cloud”</strong>. Our Azure Cloud migration services are technologically advanced
+            delivering cutting-edge solutions. Experience our seamless Azure migrations which are fast and secured.
+            TabExp’s exclusive Cloud management plans for any given infrastructure are designed to achieve perfection.
+            Our Azure Migration strategies can drive through complexities in your Cloud Migration journey. Microsoft
+            Azure pricing is modeled to improve your financial flexibility aligning your organizational changes with the
+            help of TabExp’s Azure consultants.
+        </p>
+        <h3 style="text-decoration: underline;">Infrastructure Migration to Azure</h3>
+        <p style="padding:27px">As Azure DevOps consultants we offer the best infrastructure migration strategies. Our Microsoft Azure can
+            run multifaceted IT infrastructure with power full Azure Migration tools and solutions.   Compliances
+            (HIPAA, GDPR, FedRAMP, SOX, FISMA) is addressed either by initiating fresh implementation of PCI/DSS infra
+            or by revisiting the existing architecture.</p>
+        <h3 style="text-decoration: underline;">Application Migration to Azure</h3>
+        <p style="padding:27px">
+            TabExp’s application migration to Azure Cloud is developed and deployed through accurate evaluation of
+            on-premise applications; Our Azure managed applications with end-to-end Cloud migration services. We ensure
+            fast and unmatched security for Legacy Application, Modernization, and standardization of API.
+        </p>
+        <h3 style="text-decoration: underline;">Database Migration to Azure</h3>
+        <p style="padding:27px">
+            Move multiple databases to Azure through TabExp’s Azure database migration services. We enable migration of
+            your on-premise databases to Azure by simplifying and automating the migration process. Apart from this Big
+            Data / SAP Migration, Oracle Db Migration, and SQL DB Migration are also executed with minimal downtime.
+        </p>
+        <h3 style="text-decoration: underline;">Azure DevOps Migration</h3>
+        <p style="padding:27px">
+            We help you migrate to Cloud using Azure DevOps with TabExp’s Azure Cloud services which are reliable and
+            scalable.  Our DevOps consulting and DevOps implementation processes accelerate Azure Cloud migration
+            adapting to best practices.
+        </p>
+        <h2 style="color: red;">ABOUT US</h2>
+        <h3>
+            Microsoft Azure Migration Services
+        </h3>
+        <p style="padding:27px">
+            Established to provide the best in the Cloud migration service industry, TabExp emerges as one of the top
+            line Microsoft Azure Migration consultants. Optimizing the digital transformation, the company over a period
+            of time has evolved as a hybrid and cognitive Azure Cloud migration specialist. The team has adapted to
+            modern Cloud Azure plans to enhance security and feasibility. The company with its panel of Azure DevOps
+            consultants has achieved the repute of a scalable, accessible, and a dependable Azure Cloud service
+            provider.
+            TabExp’s deployment and developmental procedures are powered by Azure migration tools and solutions assuring
+            to align with core business goals. The company implements seamless and secured migration to Azure Cloud. A
+            team of certified professional expertise to provide hassle-free and optimized Azure Cloud migrations
+            services at reduced cost. Complex workloads are handled by high-performance Azure DevOps strategies making
+            Cloud migration fast and secured.
+        </p>
+        <img src="" alt="picture2" />
+        <h2 style="color: red;">WE ARE</h2>
+        <h1 style="text-align: center;">Azure Migration Specialist</h1>
+        <img src="" alt="picture3" />
+        <p style="padding:27px">TabExp exercises its Cloud computing services exclusively as a Microsoft Azure migration specialist. With its
+            superior performance, the team has time and again proved to unlock Cloud potential using Azure DevOps
+            strategies exceeding clients’ expectations. As an Azure specialist, the company has built hybrid solutions
+            by optimizing digital transformation.</p>
+    </div>
+    );
+};
+
+export default maincontent;
+
